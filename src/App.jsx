3c5a2a9508c9cccbbd13221d0729b73a7326c49d
giulia-buonasperanza <giulia.9402@gmail.com>
2026-05-04 14:58:56 +0200

    inizializzazione progetto

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import HelloClasse154 from "./components/HelloClasse154";
+
+function App() {
+  return (
+    <h1>Blog React</h1>
+  );
+}
+export default App;
---
+++ src/components/data/articles.js
@@ -0,0 +1,27 @@
+const articles = [
+    {
+        id:1,
+        title: "Best books to read in 2026",
+        content: "Here are the best books to read in 2026..."
+    },
+    {
+        id:2,
+        title: "Top 10 travel destinations for 2026",   
+        content: "Discover the top 10 travel destinations for 2026..."
+    },
+    {       
+        id:3,
+        title: "Healthy recipes for a balanced diet",
+        content: "Try these healthy recipes for a balanced diet..."
+    },
+    {
+        id:4,
+        title: "The future of technology in 2026",
+        content: "Explore the future of technology in 2026..."  
+    },
+    {
+        id:5,
+        title: "How to stay productive while working from home",
+        content: "Learn how to stay productive while working from home..."
+    } 
+]
